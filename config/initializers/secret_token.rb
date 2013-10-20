5cc85432e1a4971774e30444e866e7e3e3f526bd
jtrope89 <jonathan@jonathantrope.com>
2013-10-20 11:25:52 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HealthyChoicesApp::Application.config.secret_key_base = '88a3e55ebe59ba4806e28e7286eec1eea3646d5a3b5f36021752521d6b74c1a7c0a90afda19bc4384cd527bf7b71f71f1113499e8b8cd7217fc9584c1a9967f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HealthyChoicesApp::Application.config.session_store :cookie_store, key: '_healthy_choices_app_session'
