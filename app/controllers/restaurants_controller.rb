62a5747be110897e234ab9a1aa16731fd85cbc86
jtrope89 <jonathan@jonathantrope.com>
2013-10-28 15:28:53 -0400

    added yelp logic to model

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -27,24 +27,5 @@ class RestaurantsController < ApplicationController
   def safe_restaurant_params
     params.require('restaurant').permit(:name, :longitude, :latitude, :city)
   end
-
-  def yelp_call(query)
-    consumer_key = 'Jdy8fp6RC-3uO9eeh1K6IA'
-    consumer_secret = 'jfO1O9GH0eMamjUhZZa9byq82ho'
-    token = '6_s2AkZmUyYEuwGsJBvkzZlkdiigc7sP'
-    token_secret = 'ahhR0jRAKYVyaHBXGamMQCAY3yw'
-
-    api_host = 'api.yelp.com'
-
-    consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://api.yelp.com"})
-    access_token = OAuth::AccessToken.new(consumer, token, token_secret)
-
-    path = "/v2/search?term=#{query}&location=new%20york&limit=1"
-
-    access_token.get(path).body
-  end
-
-  def uri_friendly(search)
-    
-  end
+  
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,25 @@
+require 'net/http'
+
 class Restaurant < ActiveRecord::Base
+
   validates :name, :city, presence: true
   acts_as_gmappable :process_geocoding => false
+  before_save :yelpify
   
+  def yelpify
+    consumer_key = 'Jdy8fp6RC-3uO9eeh1K6IA'
+    consumer_secret = 'jfO1O9GH0eMamjUhZZa9byq82ho'
+    token = '6_s2AkZmUyYEuwGsJBvkzZlkdiigc7sP'
+    token_secret = 'ahhR0jRAKYVyaHBXGamMQCAY3yw'
+
+    api_host = 'api.yelp.com'
+
+    consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://api.yelp.com"})
+    access_token = OAuth::AccessToken.new(consumer, token, token_secret)
+
+    uri_encoded_name = URI.escape("#{self.name}")
+    path = "/v2/search?term=#{uri_encoded_name}&location=new%20york&limit=1"
+    
+    self.address = access_token.get(path).body
+  end
 end
