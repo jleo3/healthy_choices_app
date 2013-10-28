3e5e82d007de6aa30a668389c480cd38a7435c0d
jtrope89 <jonathan@jonathantrope.com>
2013-10-28 14:24:20 -0400

    Adding presence validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ActiveRecord::Base
+  validates :name, :city, presence: true
   acts_as_gmappable :process_geocoding => false
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131024214720) do
+ActiveRecord::Schema.define(version: 20131028175334) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20131024214720) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "gmaps"
+    t.string   "address"
+    t.string   "city"
   end
 
   create_table "users", force: true do |t|
