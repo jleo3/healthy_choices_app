88109bf49ca81ca96ce174689c201fdcf7e25ffc
jtrope89 <jonathan@jonathantrope.com>
2013-10-29 20:49:06 -0400

    yelpifying this motha

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -19,11 +19,32 @@ class Restaurant < ActiveRecord::Base
 
     uri_encoded_name = URI.escape("#{self.name}")
     uri_encoded_city = URI.escape("#{self.city}")
-    path = "/v2/search?term=#{uri_encoded_name}&location=#{uri_encoded_city}&limit=1&category_filter=restaurants"
+    path = "/v2/search?term=#{uri_encoded_name}&location=#{uri_encoded_city}&limit=5"
     
-
+    #parse API call into JSON object
     res = JSON::parse(access_token.get(path).body)
-    self.address = res
+
+    self.address = store_restaurants_data(res).join(" ")
 
   end
-end
+
+  #stores salient restaurant data from json feed into hashes within an array within a dream
+    def store_restaurants_data(rest_obj)
+      results_array = Array.new
+      
+      rest_obj.each do |key, val|
+               
+        if key == "businesses"
+          rest_obj["businesses"].each do |business|
+            biz_hash = Hash.new
+            biz_hash["name"] = business["name"]
+            results_array << biz_hash
+          end
+        elsif key == "region"
+          
+        end
+        
+      end #end of loop for rest_obj hash
+      results_array
+    end
+  end
