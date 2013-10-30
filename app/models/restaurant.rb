2379858698841954c3bd04a8eee1b7e6c5501e5b
jtrope89 <jonathan@jonathantrope.com>
2013-10-30 17:27:56 -0400

    Fixing model parameters for gmaps4rails

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,9 +3,13 @@ require 'net/http'
 class Restaurant < ActiveRecord::Base
 
   validates :name, :city, presence: true
-  acts_as_gmappable :process_geocoding => false
   before_save :yelpify
-      
+  acts_as_gmappable validation: false
+  
+  def gmaps4rails_address
+    "#{self.address}, #{self.city}, #{self.state}, #{self.zip}"
+  end
+
   def yelpify
     consumer_key = 'Jdy8fp6RC-3uO9eeh1K6IA'
     consumer_secret = 'jfO1O9GH0eMamjUhZZa9byq82ho'
@@ -25,23 +29,25 @@ class Restaurant < ActiveRecord::Base
     res = JSON::parse(access_token.get(path).body)
 
     self.name = store_restaurants_data(res).first.biz_name
-
-    #Exclude neigberhood value in return array
-    self.address = "#{store_restaurants_data(res).first.address.first} #{store_restaurants_data(res).first.address[-1]}" 
+    self.address = store_restaurants_data(res).first.address
+    self.state = store_restaurants_data(res).first.state
+    self.zip = store_restaurants_data(res).first.zip
     self.yelp_rating = store_restaurants_data(res).first.rating
   end
 
   #stores salient restaurant data from json feed into hashes within an array within a dream
   def store_restaurants_data(rest_obj)
     my_places = []
-    my_place = Struct.new("Place", :biz_name, :rating, :address) #Defining a new struct (class) called my_place      
-
+    my_place = Struct.new("Place", :biz_name, :rating, :address, :state, :zip) #Defining a new struct (class) called my_place      
+    
     rest_obj["businesses"].each do |business|
       new_place = my_place.new
       new_place.biz_name = business["name"]
       new_place.rating = business["rating"]
-      new_place.address = business["location"]["display_address"]
-
+      new_place.address = business["location"]["address"].join(" ")
+      new_place.state = business["location"]["state_code"]
+      new_place.zip = business["location"]["postal_code"]
+      
       my_places << new_place
     end
     my_places
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131030151151) do
+ActiveRecord::Schema.define(version: 20131030211639) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20131030151151) do
     t.string   "address"
     t.string   "city"
     t.integer  "yelp_rating"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "state"
+    t.string   "zip"
   end
 
   create_table "users", force: true do |t|
