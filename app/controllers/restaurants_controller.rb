b6a87b027f74ff5187e3cc8e7724c5590a524316
jtrope89 <jonathan@jonathantrope.com>
2013-11-13 10:38:35 -0500

    Adding infowindow to index page map

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -26,6 +26,8 @@ class RestaurantsController < ApplicationController
     @hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
       marker.lat restaurant.latitude
       marker.lng restaurant.longitude
+      marker.infowindow restaurant.address
+      marker.json({ title: restaurant.name })
     end
   end
 
